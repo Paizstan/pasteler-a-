ae5a76ecc13e380ef3438a03de9f397ecf8ce606
Bryan Serrano <se24001@itcha.edu.sv.com>
2025-08-17 23:20:12 -0600

    Algo

--- SistemadePasteleria/Models/PasteldbContext.cs
+++ SistemadePasteleria/Models/PasteldbContext.cs
@@ -31,7 +31,7 @@ public partial class PasteldbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-J5K6C0NI;Database=pasteldb;Trusted_Connection=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Data Source=SQL1004.site4now.net;Initial Catalog=db_abd25b_pasteldb;User Id=db_abd25b_pasteldb_admin;Password=Newcoconela1");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
